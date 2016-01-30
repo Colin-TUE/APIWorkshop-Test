a55491c47573f257b88c99342478bd17cf6407cb
Colin-TUE <c.lambrechts@tue.nl>
2016-01-30 19:42:20 +0100

    Fixed issue 4 and removed issue 3

--- Team4/Team4-Compiled/TestCaseCircEval/src/AndGate2.java
+++ Team4/Team4-Compiled/TestCaseCircEval/src/AndGate2.java
@@ -3,23 +3,12 @@ import CircEval.LogicGate;
 
 public class AndGate2 extends AndGate {
 	
-	private boolean isBool = true;
-	
 	public AndGate2(LogicGate A, LogicGate B) {
 		super(A, B);
-		isBool = true;
-	}
-
-	//this is for the old API
-	//@Override
-	public boolean evaluate() {
-		return isBool;
 	}
 	
-	//this is for the new API
-	/*public double evaluate() {
-		isBool = true;
-		return 1.0;
-	}*/
+	public Object evaluateObject() {
+		return (Object) super.evaluate();
+	}
 	
 }
